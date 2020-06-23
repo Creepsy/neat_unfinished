3cb559bf002e7b3538b2e43b24251f146a6f6500
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-06-24 00:14:32 +0200

    extended test file with 4-4 network

--- src/test.cpp
+++ src/test.cpp
@@ -3,8 +3,9 @@
 
 int main(int argc, char const *argv[])
 {
-    genome g = genome(2, 2);
-    auto outputs = g.run({1, 2});
+    genome g = genome(4, 4);
+
+    auto outputs = g.run({1, 2, 3, 4});
     print_vector(std::cout, outputs) << std::endl;
 
     return 0;
