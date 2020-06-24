3dc2625022cf8723f2f95718bd97d571bd95fcb3
Creepsy <Robbe-online@gmx.de>
2020-06-24 09:32:48 +0200

    added some method definitions to genome

--- src/genome.h
+++ src/genome.h
@@ -14,20 +14,18 @@ struct connection
 class genome
 {
 private:
-    std::map<size_t, std::vector<connection>> connections;
-    std::map<size_t, std::vector<connection>> reverse_connections;
+    std::map<size_t, std::map<size_t, connection>> connections;
+    std::map<size_t, std::map<size_t, connection>> reverse_connections;
     size_t input_size, output_size;
 
 public:
     genome(size_t input_size, size_t outputs);
     ~genome();
     void add_connection(const connection &c);
-    size_t get_input_id(size_t input);
-    size_t get_output_id(size_t output);
-    size_t from_input_id(size_t input);
-    size_t from_output_id(size_t output);
-    bool is_input_id(size_t input);
-    bool is_output_id(size_t output);
     std::vector<double> run(const std::vector<double> &inputs);
     double compute_node_result(size_t node, const std::vector<double> &inputs);
+    void mutate();
+    void mutate_weights();
+    void split_connection(cosnt size_t& from, const size_t& to);
+    void connect(const size_t& first_node, const size_t& second_node);
 };
